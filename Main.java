472c302dab0bee44946e185f1fe0ef2a83e09484
annisaalivia <annisaaliviia@gmail.com>
2021-10-23 13:27:44 +0700

    Challenge 5

---
+++ Main.java
@@ -0,0 +1,46 @@
+class Main {
+    public static void main(String[] args) {
+      Heroes hero = new Heroes();
+  
+      hero.setProfile("Annisa", 19, 158);
+      hero.profile();
+      
+  
+      // jalankan aktivitas disini...
+      hero.jalan();
+      hero.jalan();
+
+      hero.lari();
+      hero.lari();
+      hero.lari(); 
+      hero.lari();
+
+      hero.makan();
+      hero.makan();
+
+      hero.minum();
+      hero.minum();
+
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+
+      hero.duduk();
+      hero.duduk();
+      hero.duduk();
+      hero.duduk();
+      
+      // tampilkan status terkini...
+      hero.profile();
+
+       // challenge:
+        /*
+        Group 6: jalan 2x, lari 4x, makan 2x, minum 2x, lompat 7x, duduk 4x
+        */
+      
+    }
+  }
